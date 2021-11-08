eafca7e7f5dcf0296e73eeb75e49effa16b586f0
Fedor <fedor531@yandex.ru>
2021-11-08 19:36:54 +0200

    feat: add

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import pages from './modules/pagesState'
-import answers from './modules/answers'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        pages,
-        answers
+        pages
     }
 })
